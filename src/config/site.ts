b36e8a77bd028fdb8ec44087a4103ed94fb46493
seonwooj0810 <seonwooj0810@gmail.com>
2026-09-25 13:54:16 +0900

    chore: 폐기된 to-be-healthy.shop 링크를 geonganghaejim.site로 바꾼다

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
   title: '건강해짐',
   name: '건강해짐',
-  url: 'https://www.to-be-healthy.shop',
+  url: 'https://geonganghaejim.site',
   ogImage: '/images/banner.png',
   description: '피트니스 센터, 트레이너와 회원을 위한 필수 앱',
   locale: 'ko_KR',
