05b8a1dd4add1f257f20fab0ba9e221dd8195746
seonwooj0810 <seonwooj0810@gmail.com>
2026-09-26 14:37:13 +0900

    chore(landing): 문의 메일과 Play 스토어 링크를 새 식별자로 바꾼다
    문의 메일을 seonwooj0810@gmail.com으로, Play 스토어 링크를 앱의 현재 applicationId
    (com.geonganghaejim.app)로 교체한다.

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,7 +1,7 @@
 export const appConfig = {
-  csEmail: 'tobehealthy0127@gmail.com',
+  csEmail: 'seonwooj0810@gmail.com',
   playStoreLink:
-    'https://play.google.com/store/apps/details?id=site.tobehealthy.webview&pcampaignid=web_share',
+    'https://play.google.com/store/apps/details?id=com.geonganghaejim.app&pcampaignid=web_share',
   appStoreLink:
     'https://apps.apple.com/kr/app/%EA%B1%B4%EA%B0%95%ED%95%B4%EC%A7%90-pt-%EA%B4%80%EB%A6%AC/id6794697483',
   termsLink: 'https://geonganghaegym.junghaebom.com/policy/terms',
